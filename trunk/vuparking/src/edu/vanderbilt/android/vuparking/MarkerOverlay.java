64fc8be02c5a61fd81576af8b9136fee4df68d6f
lixwbupt <lixwbupt@ba21aae4-09fa-11df-bd4f-1fc3cb39d6f8>
2010-03-01 04:18:01 +0000

    B5 Refactored zone overlay and marker overlay.

---
+++ trunk/vuparking/src/edu/vanderbilt/android/vuparking/MarkerOverlay.java
@@ -0,0 +1,127 @@
+/*****************************************************************************
+ *   Copyright VUparking for CS279 project                                   *
+ *   Developed by Xiaowei Li, Yuan Zhuang                                    *
+ *   Licensed under the Apache License, Version 2.0 (the "License");         *
+ *   you may not use this file except in compliance with the License.        *
+ *   You may obtain a copy of the License at                                 *
+ *                                                                           *
+ *   http://www.apache.org/licenses/LICENSE-2.0                              *
+ *                                                                           *
+ *   Unless required by applicable law or agreed to in writing, software     *
+ *   distributed under the License is distributed on an "AS IS" BASIS,       *
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.*
+ *   See the License for the specific language governing permissions and     *
+ *   limitations under the License.                                          *
+ ****************************************************************************/
+
+// This file is written for querying parking lot information from DB and displaying marker on map.
+
+package edu.vanderbilt.android.vuparking;
+
+import java.util.ArrayList;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.widget.Toast;
+
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.ItemizedOverlay;
+import com.google.android.maps.OverlayItem;
+
+public class MarkerOverlay extends ItemizedOverlay<OverlayItem> {
+
+	private ArrayList<OverlayItem> mOverlays = new ArrayList<OverlayItem>();
+	private ParkingDBManager parkingDb = new ParkingDBManager();    // Access local database.
+	private int num_areas = 6;
+	private Context mContext;
+	ParkingMap map;
+	
+	public MarkerOverlay(ParkingMap parkingmap, Context context) {
+		super(boundCenterBottom(context.getResources().getDrawable(R.drawable.parking)));
+		mContext = context;
+		map = parkingmap;
+		// Get corresponding parking lots information for each selected zone from Database.
+		addAllOverlayItems();
+	}
+	
+	// Add overlay item.
+	public void addOverlay(OverlayItem overlay) {
+	    mOverlays.add(overlay);
+	    populate();
+	}
+	
+	// Called by populate().
+	@Override
+	protected OverlayItem createItem(int i) {
+	  return mOverlays.get(i);
+	}
+	
+	// Query database to add all parking markers on map.
+	public void addAllOverlayItems() {
+		if (parkingDb.openDB()) {
+			for (int i = 0; i < num_areas; i++) {
+				if (map.zoneToDisplay[i]) {
+					ArrayList<ParkingLot> lots = parkingDb.queryParkingZone(i);
+					for (int j = 0; j < lots.size(); j++)
+			        {
+			        	double lat = lots.get(j).getLatitude();
+			        	double lng = lots.get(j).getLongtitude();
+			        	GeoPoint p = new GeoPoint((int)(lat*1E6),(int)(lng*1E6));
+			        	OverlayItem overlay = new OverlayItem(p, lots.get(j).getName(), Integer.toString(lots.get(j).getId()));
+			        	overlay.setMarker(boundCenterBottom(mContext.getResources().getDrawable(R.drawable.garage_marker)));
+			        	addOverlay(overlay);
+			        }
+				}
+			}
+		}
+		else Toast.makeText(mContext, "Database open failed!", Toast.LENGTH_LONG).show();
+	}
+	
+	// Define the action when click on the marker to show detailed information.
+	@Override
+	protected boolean onTap(int index) {
+	  OverlayItem item = mOverlays.get(index);
+	  // map.showDialog(0); // For refreshing data.
+	  AlertDialog.Builder dialog = new AlertDialog.Builder(mContext);
+	  dialog.setIcon(R.drawable.garages);
+	  dialog.setTitle(item.getTitle());
+	  int lotId = Integer.parseInt(item.getSnippet());
+	  if (parkingDb.openDB()) {
+		  ParkingLot p = parkingDb.queryParkingById(lotId);
+		  dialog.setMessage("Address: " + p.getAddress() + 
+				            "\nCapacity: " + Integer.toString(p.getNumSpot()) +
+				            "\nAvailable: " + Integer.toString(p.getNumAvailabe()) + 
+				            "\nDisable: " + Integer.toString(p.getNumDisable()));
+	  }
+	  else Toast.makeText(mContext, "Database open failed!", Toast.LENGTH_LONG).show();
+	  
+	  // Create dialog for next step.
+	  dialog.setPositiveButton("Direction", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int which) {
+				// ToDirection();
+				Toast.makeText(mContext, "Service currently unavailable!", Toast.LENGTH_LONG).show();
+			}
+		});
+	  
+	  // Contact remote server to get the latest information.
+	  dialog.setNeutralButton("Refresh", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int which) {
+				
+			}
+		});
+	  
+	  dialog.setNegativeButton("Back", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int which) {
+				dialog.dismiss();
+			}
+		});
+	  dialog.show();
+	  return true;
+	}
+
+	@Override
+	public int size() {
+	  return mOverlays.size();
+	}
+}
---
+++ trunk/vuparking/src/edu/vanderbilt/android/vuparking/ZoneOverlay.java
@@ -0,0 +1,125 @@
+/*****************************************************************************
+ *   Copyright VUparking for CS279 project                                   *
+ *   Developed by Xiaowei Li, Yuan Zhuang                                    *
+ *   Licensed under the Apache License, Version 2.0 (the "License");         *
+ *   you may not use this file except in compliance with the License.        *
+ *   You may obtain a copy of the License at                                 *
+ *                                                                           *
+ *   http://www.apache.org/licenses/LICENSE-2.0                              *
+ *                                                                           *
+ *   Unless required by applicable law or agreed to in writing, software     *
+ *   distributed under the License is distributed on an "AS IS" BASIS,       *
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.*
+ *   See the License for the specific language governing permissions and     *
+ *   limitations under the License.                                          *
+ ****************************************************************************/
+
+// This file is written for covering zone areas with different colors.
+
+package edu.vanderbilt.android.vuparking;
+
+import java.util.ArrayList;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Point;
+
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapView;
+
+class ZoneOverlay extends com.google.android.maps.Overlay {
+	
+	private int num_areas = 6;
+	ArrayList<ArrayList<coordinate>> ZonePt = new ArrayList<ArrayList<coordinate>>();
+	ParkingMap map;
+	
+    // Define struct for coordinate.
+    class coordinate
+    {
+    	int x, y;
+    	coordinate(int a, int b) { x = a; y = b;}
+    	int getX() { return x;}
+    	int getY() { return y;}
+    }
+    
+    public ZoneOverlay(ParkingMap parkingmap) {
+    	map = parkingmap;
+    }
+	
+	// Add all area vertices.
+	public void loadZoneTopo() {
+		// Zone 1;
+		ArrayList<coordinate> pt1 = new ArrayList<coordinate>();
+        pt1.add(new coordinate(36144044,-86799781));
+        pt1.add(new coordinate(36137493,-86800640));
+        pt1.add(new coordinate(36137043,-86795619));
+        pt1.add(new coordinate(36143385,-86794417));
+        ZonePt.add(pt1);
+        // Zone 2;
+        ArrayList<coordinate> pt2 = new ArrayList<coordinate>();
+        pt2.add(new coordinate(36145950,-86799610));
+        pt2.add(new coordinate(36148029,-86799309));
+        pt2.add(new coordinate(36150524,-86800940));
+        pt2.add(new coordinate(36147890,-86806347));
+        pt2.add(new coordinate(36144044,-86803730));
+        ZonePt.add(pt2);
+        // Zone 3;
+        ArrayList<coordinate> pt3 = new ArrayList<coordinate>();
+        pt3.add(new coordinate(36138741,-86810896));
+        pt3.add(new coordinate(36143004,-86810081));
+        pt3.add(new coordinate(36146123,-86809824));
+        pt3.add(new coordinate(36147890,-86806347));
+        pt3.add(new coordinate(36144044,-86803730));
+        pt3.add(new coordinate(36138013,-86804802));
+        ZonePt.add(pt3);
+        // Zone 4;
+        ArrayList<coordinate> pt4 = new ArrayList<coordinate>();
+        ZonePt.add(pt4);
+        // Medical;
+        ArrayList<coordinate> pt5 = new ArrayList<coordinate>();
+        pt5.add(new coordinate(36138013,-86804802));
+        pt5.add(new coordinate(36144044,-86803730));
+        pt5.add(new coordinate(36145950,-86799610));
+        pt5.add(new coordinate(36137597,-86800640));
+        ZonePt.add(pt5);
+        // Visitor;
+        ArrayList<coordinate> pt6 = new ArrayList<coordinate>();
+        ZonePt.add(pt6);
+	}
+	
+	// Draw polygon with color on specific zones.
+	 public boolean draw (Canvas canvas, MapView mv, boolean shadow, long when) {
+		
+		super.draw(canvas, mv, shadow);
+        int opaque = 50; // 0: transparent, 255: black
+        int[] color = {Color.YELLOW, Color.CYAN, Color.GREEN, Color.BLUE, Color.MAGENTA, Color.TRANSPARENT};
+
+        loadZoneTopo();
+        
+        for (int i=0; i < num_areas; i++) {
+        	if (map.zoneToDisplay[i]) {
+        		Path path = new Path();
+                if (ZonePt.get(i).size() != 0)
+                {
+                	Point pt = new Point();
+                	mv.getProjection().toPixels(new GeoPoint(ZonePt.get(i).get(0).getX(),ZonePt.get(i).get(0).getY()), pt);
+                	path.moveTo(pt.x, pt.y);
+                    for (int j = 1; j < ZonePt.get(i).size(); j++)
+                    {
+                    	mv.getProjection().toPixels(new GeoPoint(ZonePt.get(i).get(j).getX(),ZonePt.get(i).get(j).getY()), pt);
+                    	path.lineTo(pt.x, pt.y);
+                    }
+                }
+                Paint paint = new Paint();
+                paint.setColor(color[i]);
+                paint.setAlpha(opaque);
+                paint.setStyle(Paint.Style.FILL);
+                canvas.drawPath(path, paint);
+        	}
+        }
+        return true;
+	 }
+}	
+	 
