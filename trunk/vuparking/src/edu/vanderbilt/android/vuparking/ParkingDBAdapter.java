7e3d58032085d9570ee144939fe6eb5380b26dd7
yuanzh14 <yuanzh14@ba21aae4-09fa-11df-bd4f-1fc3cb39d6f8>
2010-03-04 03:23:11 +0000

    B7 Code reformatted. US3:1 Modified onCreateDialog for multiple zone selection. B13 Code unrelated to user stories deleted.

--- trunk/vuparking/src/edu/vanderbilt/android/vuparking/ParkingDBAdapter.java
+++ trunk/vuparking/src/edu/vanderbilt/android/vuparking/ParkingDBAdapter.java
@@ -26,182 +26,186 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
-public class ParkingDBAdapter {
-	
-    public static final String KEY_ROWID = "_id";
-    public static final String KEY_TYPE = "type";
-    public static final String KEY_ZONE = "zone";
-    public static final String KEY_NAME = "name";
-    public static final String KEY_ADDRESS = "address";
-    public static final String KEY_COORDINATE_X = "coordinate_x";
-    public static final String KEY_COORDINATE_Y = "coordinate_y";
-    public static final String KEY_CAPACITY = "capacity";
-    public static final String KEY_AVAILABLE = "available";
-    public static final String KEY_DISABLE = "disable";
-    public static final String KEY_RATE = "rate";
-    
-    private static final String TAG = "ParkingDBAdapter";
-    
-    private static final String DATABASE_NAME = "vuparking.db";
-    private static final String DATABASE_TABLE = "ParkingLot";
-    private static final int DATABASE_VERSION = 1;
+public class ParkingDBAdapter 
+{
+	public static final String KEY_ROWID = "_id";
+	public static final String KEY_TYPE = "type";
+	public static final String KEY_ZONE = "zone";
+	public static final String KEY_NAME = "name";
+	public static final String KEY_ADDRESS = "address";
+	public static final String KEY_COORDINATE_X = "coordinate_x";
+	public static final String KEY_COORDINATE_Y = "coordinate_y";
+	public static final String KEY_CAPACITY = "capacity";
+	public static final String KEY_AVAILABLE = "available";
+	public static final String KEY_DISABLE = "disable";
+	public static final String KEY_RATE = "rate";
 
-    private static final String DATABASE_CREATE =
-        "create table if not exists ParkingLot (_id integer primary key, "
-        + "type integer not null, zone integer not null, "
-        + "name text not null, address text, "
-        + "coordinate_x text not null, coordinate_y text not null, "
-        + "capacity integer not null, available integer not null,"
-        + "disable integer not null, rate integer);";
-        
-    private final Context context;    
-    private DatabaseHelper DBHelper;
-    
-    // Handle of database instance.
-    private SQLiteDatabase db;
+	private static final String TAG = "ParkingDBAdapter";
 
-    public ParkingDBAdapter(Context ctx)
-    {
-        this.context = ctx;
-        //db = context.openOrCreateDatabase(DATABASE_NAME, DATABASE_VERSION, null)
-        DBHelper = new DatabaseHelper(context);
-    }
-        
-    private static class DatabaseHelper extends SQLiteOpenHelper 
-    {
-        DatabaseHelper(Context context) 
-        {
-            super(context, DATABASE_NAME, null, DATABASE_VERSION);
-        }
+	private static final String DATABASE_NAME = "vuparking.db";
+	private static final String DATABASE_TABLE = "ParkingLot";
+	private static final int DATABASE_VERSION = 1;
 
-        @Override
-        public void onCreate(SQLiteDatabase db)
-        {
-        	Log.i("i", "Creating Database");
-            db.execSQL(DATABASE_CREATE);
-        }
+	private static final String DATABASE_CREATE =
+		"create table if not exists ParkingLot (_id integer primary key, "
+		+ "type integer not null, zone integer not null, "
+		+ "name text not null, address text, "
+		+ "coordinate_x text not null, coordinate_y text not null, "
+		+ "capacity integer not null, available integer not null,"
+		+ "disable integer not null, rate integer);";
 
-        @Override
-        public void onUpgrade(SQLiteDatabase db, int oldVersion, 
-        int newVersion)
-        {
-            Log.w(TAG, "Upgrading database from version " + oldVersion 
-                    + " to "
-                    + newVersion + ", which will destroy all old data");
-            db.execSQL("DROP TABLE IF EXISTS ParkingLot");
-            onCreate(db);
-        }
-    }    
-    
-    // opens the database.
-    public ParkingDBAdapter open() throws SQLException 
-    {
-        db = DBHelper.getWritableDatabase();
-        return this;
-    }
+	private final Context context;    
+	private DatabaseHelper DBHelper;
 
-    // closes the database.  
-    public void close() 
-    {
-    	Log.i("i", "Closing Database");
-        DBHelper.close();
-    }
-    
-    // Check if there exist parking data.
-    public boolean checkDb()
-    {
-    	Cursor c = db.rawQuery("select * from ParkingLot", null);
-    	int count = c.getCount();
-    	c.close();
-    	if (count != 0) return true;
-    	else return false;
-    }    
-    
-    // Insertion: insert a new parking lot into database, return -1 to indicate failure.
-    public long insertLot(ParkingLot p)
-    {
-        ContentValues initialValues = new ContentValues();
-        initialValues.put(KEY_ROWID, p.getId());
-        initialValues.put(KEY_TYPE, p.getType());
-        initialValues.put(KEY_ZONE, p.getZone());
-        initialValues.put(KEY_NAME, p.getName());
-        initialValues.put(KEY_ADDRESS, p.getAddress());
-        initialValues.put(KEY_COORDINATE_X, Double.toString(p.getLatitude()));
-        initialValues.put(KEY_COORDINATE_Y, Double.toString(p.getLongtitude()));
-        initialValues.put(KEY_CAPACITY, p.getNumSpot());
-        initialValues.put(KEY_AVAILABLE, p.getNumAvailabe());
-        initialValues.put(KEY_DISABLE, p.getNumDisable());
-        initialValues.put(KEY_RATE, p.getRate());
-        return db.insert(DATABASE_TABLE, null, initialValues);
-    }
-    
+	// Handle of database instance.
+	private SQLiteDatabase db;
+
+	public ParkingDBAdapter(Context ctx)
+	{
+		this.context = ctx;
+		DBHelper = new DatabaseHelper(context);
+	}
+
+	private static class DatabaseHelper extends SQLiteOpenHelper 
+	{
+		DatabaseHelper(Context context) 
+		{
+			super(context, DATABASE_NAME, null, DATABASE_VERSION);
+		}
+
+		@Override
+		public void onCreate(SQLiteDatabase db)
+		{
+			Log.i("i", "Creating Database");
+			db.execSQL(DATABASE_CREATE);
+		}
+
+		@Override
+		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+		{
+			Log.w(TAG, "Upgrading database from version " + oldVersion 
+					+ " to "
+					+ newVersion + ", which will destroy all old data");
+			db.execSQL("DROP TABLE IF EXISTS ParkingLot");
+			onCreate(db);
+		}
+	}    
+
+	// opens the database.
+	public ParkingDBAdapter open() throws SQLException 
+	{
+		db = DBHelper.getWritableDatabase();
+		return this;
+	}
+
+	// closes the database.  
+	public void close() 
+	{
+		Log.i("i", "Closing Database");
+		DBHelper.close();
+	}
+
+	// Check if there exist parking data.
+	public boolean checkDb()
+	{
+		Cursor c = db.rawQuery("select * from ParkingLot", null);
+		int count = c.getCount();
+		c.close();
+		if (count != 0) return true;
+		else return false;
+	}    
+
+	// Insertion: insert a new parking lot into database, return -1 to indicate failure.
+	public long insertLot(ParkingLot p)
+	{
+		ContentValues initialValues = new ContentValues();
+		initialValues.put(KEY_ROWID, p.getId());
+		initialValues.put(KEY_TYPE, p.getType());
+		initialValues.put(KEY_ZONE, p.getZone());
+		initialValues.put(KEY_NAME, p.getName());
+		initialValues.put(KEY_ADDRESS, p.getAddress());
+		initialValues.put(KEY_COORDINATE_X, Double.toString(p.getLatitude()));
+		initialValues.put(KEY_COORDINATE_Y, Double.toString(p.getLongtitude()));
+		initialValues.put(KEY_CAPACITY, p.getNumSpot());
+		initialValues.put(KEY_AVAILABLE, p.getNumAvailabe());
+		initialValues.put(KEY_DISABLE, p.getNumDisable());
+		initialValues.put(KEY_RATE, p.getRate());
+		return db.insert(DATABASE_TABLE, null, initialValues);
+	}
+
 	// Update: update number of available and disable spots got from server.
-    // If we want to update all parking lots, we need a cycle to do this. 
-    public boolean updateLot(long rowId, int num_available, int num_disable)
-    {
-    	 ContentValues args = new ContentValues();
-         args.put(KEY_AVAILABLE, num_available);
-         args.put(KEY_DISABLE, num_disable);
-         return db.update(DATABASE_TABLE, args,
-                         KEY_ROWID + "=" + rowId, null) > 0;
-    }
+	// If we want to update all parking lots, we need a cycle to do this. 
+	public boolean updateLot(long rowId, int num_available, int num_disable)
+	{
+		ContentValues args = new ContentValues();
+		args.put(KEY_AVAILABLE, num_available);
+		args.put(KEY_DISABLE, num_disable);
+		return db.update(DATABASE_TABLE, args,
+				KEY_ROWID + "=" + rowId, null) > 0;
+	}
 
-    // Deletion: deletes a particular parking lot from database.
-    public boolean deleteLot(long rowId)
-    {
-        return db.delete(DATABASE_TABLE, KEY_ROWID + 
-        		"=" + rowId, null) > 0;
-    }
-    
-    // deletes all parking lots from database.
-    public boolean deleteAllLots(){
-    	return db.delete(DATABASE_TABLE, null, null)>0;
-    }
-    
-    // Query: retrieve parking lot information by zone.
-    public Cursor getLotsByZone(int zone){
-    	Cursor mCursor = 
-    		   db.query(false, DATABASE_TABLE, new String[] {
-    				    KEY_ROWID,
-    				    KEY_TYPE,
-                		KEY_ZONE,
-                		KEY_NAME,
-                		KEY_ADDRESS,
-                        KEY_COORDINATE_X,
-                        KEY_COORDINATE_Y,
-                        KEY_CAPACITY,
-                        KEY_AVAILABLE,
-                        KEY_DISABLE,
-                        KEY_RATE,
-                		},
-                		KEY_ZONE + "=" + zone, null, null, null, null, null);  
-    	 if (mCursor != null) {
-             mCursor.moveToFirst();
-         }
-         return mCursor;    	
-    }
-    
-    // retrieve parking lot information by id.
-    public Cursor getLotById(long rowId) throws SQLException 
-    {
-        Cursor mCursor =
-                db.query(true, DATABASE_TABLE, new String[] {
-                		KEY_ROWID,
-    				    KEY_TYPE,
-                		KEY_ZONE,
-                		KEY_NAME,
-                		KEY_ADDRESS,
-                        KEY_COORDINATE_X,
-                        KEY_COORDINATE_Y,
-                        KEY_CAPACITY,
-                        KEY_AVAILABLE,
-                        KEY_DISABLE,
-                        KEY_RATE,
-                		}, 
-                		KEY_ROWID + "=" + rowId, null, null, null, null, null);
-        if (mCursor != null) {
-            mCursor.moveToFirst();
-        }
-        return mCursor;
-    }	
+	// Deletion: deletes a particular parking lot from database.
+	public boolean deleteLot(long rowId)
+	{
+		return db.delete(DATABASE_TABLE, KEY_ROWID + 
+				"=" + rowId, null) > 0;
+	}
+
+	// deletes all parking lots from database.
+	public boolean deleteAllLots()
+	{
+		return db.delete(DATABASE_TABLE, null, null)>0;
+	}
+
+	// Query: retrieve parking lot information by zone.
+	public Cursor getLotsByZone(int zone)
+	{
+		Cursor mCursor = 
+			db.query(false, DATABASE_TABLE, new String[]
+			{
+				KEY_ROWID,
+				KEY_TYPE,
+				KEY_ZONE,
+				KEY_NAME,
+				KEY_ADDRESS,
+				KEY_COORDINATE_X,
+				KEY_COORDINATE_Y,
+				KEY_CAPACITY,
+				KEY_AVAILABLE,
+				KEY_DISABLE,
+				KEY_RATE,
+			},
+			KEY_ZONE + "=" + zone, null, null, null, null, null);
+		
+		if (mCursor != null) 
+			mCursor.moveToFirst();
+
+		return mCursor;    	
+	}
+
+	// retrieve parking lot information by id.
+	public Cursor getLotById(long rowId) throws SQLException 
+	{
+		Cursor mCursor =
+			db.query(true, DATABASE_TABLE, new String[] 
+			{
+				KEY_ROWID,
+				KEY_TYPE,
+				KEY_ZONE,
+				KEY_NAME,
+				KEY_ADDRESS,
+				KEY_COORDINATE_X,
+				KEY_COORDINATE_Y,
+				KEY_CAPACITY,
+				KEY_AVAILABLE,
+				KEY_DISABLE,
+				KEY_RATE,
+			}, 
+			KEY_ROWID + "=" + rowId, null, null, null, null, null);
+
+		if (mCursor != null)
+			mCursor.moveToFirst();
+
+		return mCursor;
+	}	
 }
--- trunk/vuparking/src/edu/vanderbilt/android/vuparking/ParkingMap.java
+++ trunk/vuparking/src/edu/vanderbilt/android/vuparking/ParkingMap.java
@@ -31,123 +31,137 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 
-public class ParkingMap extends MapActivity {
-        
-        private static MapView mv;
-        public boolean[] zoneToDisplay;
-        
-        private LocationOverlay myLocation;
-        private int zoomLevel;
-        
-        // Called when creating the activity to show map view.
-        @Override
-        public void onCreate (Bundle savedInstanceState) {
-        	super.onCreate(savedInstanceState);
-            setContentView(R.layout.mapview);
-            
-            mv = (MapView) findViewById(R.id.map);
-            mv.setBuiltInZoomControls(true);     // For zoom in/out.
-            
-            // Get user choices from main activity.
-            Bundle bundle = this.getIntent().getExtras();
-            zoneToDisplay = bundle.getBooleanArray("ZoneChoice");
-            
-            // Set default map center and zoom level.
-            zoomLevel = 15;
-            GeoPoint p = new GeoPoint((int)(36141710),(int)(-86803669));
-	        setCenter(p, zoomLevel);
-	        
-	        myLocation = new LocationOverlay(this, mv);
-            myLocation.enableMyLocation();      // Start receiving GPS signal.
-            
-	        addAllOverlay();
-            //mapView.setStreetView(true);
-        }
-        
-        // Add overlay object onto map view.
-        private void addOverlay(Overlay object) {
-        	mv.getOverlays().add(object);
-        	mv.invalidate();
-        }
-        
-        // Add zone color, markers onto map view.
-        private void addAllOverlay() {
-        	ZoneOverlay zOverlay = new ZoneOverlay(this);
-		    addOverlay(zOverlay);
-		    
-            MarkerOverlay mOverlay = new MarkerOverlay(this,this);
-            addOverlay(mOverlay);
-        }
-        
-        // Refresh all overlays on map.
-        private void refreshOverlay() {
-        	mv.getOverlays().clear();
-        	addAllOverlay();
-        }
-    	
-    	// Set map center and zoom level.
-        private void setCenter(GeoPoint p, int zoom){
-            mv.getController().animateTo(p);
-            mv.getController().setZoom(zoom);
-        }
-        
-        private final static int MENU_ZONE = 0;
-        private final static int MENU_CURLOC = 1;
-        
-        // Define the menu items.
-        public boolean onCreateOptionsMenu (Menu menu){
-        	super.onCreateOptionsMenu(menu);
-        	menu.add(Menu.NONE, MENU_ZONE, Menu.NONE, "Select Zone");
-        	menu.add(Menu.NONE, MENU_CURLOC, Menu.NONE, "Show My Position");
-			return true;
-        }
-                
-        // Define the action when one of menu items selected.
-        public boolean onOptionsItemSelected (MenuItem item){
-        	super.onOptionsItemSelected(item);
-            switch (item.getItemId()) {
-            case MENU_ZONE:
-            	showDialog(MENU_ZONE);    // pop up zone choice dialog.
-            	break;
-            case MENU_CURLOC:
-            	addOverlay(myLocation);
-            	setCenter(myLocation.getCurLocation(), zoomLevel);
-            	if (myLocation.signal == false)
-            		Toast.makeText(this, "Displaying your default location", Toast.LENGTH_SHORT).show();
-            	break;
-            }
-        	return true;
-        }
-        
-        // Define the pop up dialog for choosing zones.
-    	protected Dialog onCreateDialog(int id){
-    		AlertDialog.Builder builder = new AlertDialog.Builder(this);
-    		CharSequence[] zones = {"Zone1", "Zone2", "Zone3", "Zone4", "Medical", "Visitor"};
-    		builder.setTitle("Select parking area");
-    		builder.setMultiChoiceItems(zones, zoneToDisplay, new DialogInterface.OnMultiChoiceClickListener() {				
-				public void onClick(DialogInterface dialog, int which, boolean isChecked) {
-					zoneToDisplay[which] = isChecked;
-				}
-    		});
-    		
-    		builder.setNeutralButton("Done", new DialogInterface.OnClickListener() {
-    			public void onClick(DialogInterface dialog, int which) {
-    				dialog.dismiss();
-    			    refreshOverlay();          // Refresh all the overlay items on map.
-    			}
-    		});
-    		builder.setNegativeButton("Back", new DialogInterface.OnClickListener() {
-    			public void onClick(DialogInterface dialog, int which) {
-    				dialog.dismiss();
-    			}
-    		});
-    		return builder.create();
-    	}
-       
-        // Needed for MapActivity.
-        @Override
-        protected boolean isRouteDisplayed()
-        {
-                return false;
-        }
+public class ParkingMap extends MapActivity 
+{
+	private static MapView mv;
+	public boolean[] zoneToDisplay;
+
+	private LocationOverlay myLocation;
+	private int zoomLevel;
+
+	private final static int MENU_ZONE = 0; 	//parking zone
+	private final static int MENU_CURLOC = 1;	//current location
+
+	// Called when creating the activity to show map view.
+	@Override
+	public void onCreate (Bundle savedInstanceState) 
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.mapview);
+
+		mv = (MapView) findViewById(R.id.map);
+		mv.setBuiltInZoomControls(true);     // For zoom in/out.
+
+		// Get user choices from main activity.
+		Bundle bundle = this.getIntent().getExtras();
+		zoneToDisplay = bundle.getBooleanArray("zoneChoices");
+
+		// Set default map center and zoom level.
+		zoomLevel = 15;
+		GeoPoint p = new GeoPoint((int)(36141710),(int)(-86803669));
+		setCenter(p, zoomLevel);
+
+		myLocation = new LocationOverlay(this, mv);
+		myLocation.enableMyLocation();      // Start receiving GPS signal.
+
+		addAllOverlay();
+	}
+
+	// Add overlay object onto map view.
+	private void addOverlay(Overlay object) 
+	{
+		mv.getOverlays().add(object);
+		mv.invalidate();
+	}
+
+	// Add zone color, markers onto map view.
+	private void addAllOverlay() 
+	{
+		ZoneOverlay zOverlay = new ZoneOverlay(this);
+		addOverlay(zOverlay);
+
+		MarkerOverlay mOverlay = new MarkerOverlay(this,this);
+		addOverlay(mOverlay);
+	}
+
+	// Refresh all overlays on map.
+	private void refreshOverlay() 
+	{
+		mv.getOverlays().clear();
+		addAllOverlay();
+	}
+
+	// Set map center and zoom level.
+	private void setCenter(GeoPoint p, int zoom)
+	{
+		mv.getController().animateTo(p);
+		mv.getController().setZoom(zoom);
+	}
+
+	// Define the menu items.
+	public boolean onCreateOptionsMenu (Menu menu)
+	{
+		super.onCreateOptionsMenu(menu);
+		menu.add(Menu.NONE, MENU_ZONE, Menu.NONE, "Select Zone");
+		menu.add(Menu.NONE, MENU_CURLOC, Menu.NONE, "Show My Location");
+		return true;
+	}
+
+	// Define the action when one of menu items selected.
+	public boolean onOptionsItemSelected (MenuItem item)
+	{
+		super.onOptionsItemSelected(item);
+
+		switch (item.getItemId()) 
+		{
+		case MENU_ZONE:
+			showDialog(MENU_ZONE);    // pop up zone choice dialog.
+			break;
+		case MENU_CURLOC:
+			addOverlay(myLocation);
+			setCenter(myLocation.getCurLocation(), zoomLevel);
+			if (myLocation.signal == false)
+				Toast.makeText(this, "Displaying your default location", Toast.LENGTH_SHORT).show();
+			break;
+		}
+		return true;
+	}
+
+	// Define the pop up dialog for choosing zones.
+	protected Dialog onCreateDialog(int id)
+	{
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		CharSequence[] zones = {"Zone1", "Zone2", "Zone3", "Zone4", "Medical", "Visitor"};
+		builder.setTitle("Please pick your zones");
+		builder.setMultiChoiceItems(zones, zoneToDisplay, new DialogInterface.OnMultiChoiceClickListener() 
+		{				
+			public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+				zoneToDisplay[which] = isChecked;
+			}
+		});
+
+		builder.setNeutralButton("Done", new DialogInterface.OnClickListener() 
+		{
+			public void onClick(DialogInterface dialog, int which) 
+			{
+				dialog.dismiss();
+				refreshOverlay();          // Refresh all the overlay items on map.
+			}
+		});
+		builder.setNegativeButton("Back", new DialogInterface.OnClickListener() 
+		{
+			public void onClick(DialogInterface dialog, int which) 
+			{
+				dialog.dismiss();
+			}
+		});
+		return builder.create();
+	}
+
+	// Needed for MapActivity.
+	@Override
+	protected boolean isRouteDisplayed()
+	{
+		return false;
+	}
 }
