6bbbe03d9190dc350d9aa8cadf177e62dcc76064
yuanzh14 <yuanzh14@ba21aae4-09fa-11df-bd4f-1fc3cb39d6f8>
2010-03-25 01:50:02 +0000

    B19: Zone3, Medical and Visitor parking lots added

--- trunk/vuparking/src/edu/vanderbilt/android/vuparking/ParkingDBManager.java
+++ trunk/vuparking/src/edu/vanderbilt/android/vuparking/ParkingDBManager.java
@@ -107,28 +107,46 @@ public class ParkingDBManager
 		lots.add(new ParkingLot(29, LOT, ZONE2, "Barbizon Apartment", "", 36.149417,-86.798821, 100, 20, 80, 0));
 		lots.add(new ParkingLot(30, LOT, ZONE2, "Barbizon Apartment", "", 36.149417,-86.798821, 100, 20, 80, 0));
 		lots.add(new ParkingLot(31, GARAGE, ZONE2, "Center Garage", "", 36.149099,-86.799315, 20, 5, 15, 0));
-		
-		//Zone 3
-		lots.add(new ParkingLot(32, LOT, ZONE3, "Olin hall", "2400 Highland Ave", 36.142937, -86.805072, 20, 0, 20, 0));
-		lots.add(new ParkingLot(33, GARAGE, ZONE3, "25th Ave Staff garage", "2499 Highland Ave", 36.142365, -86.806124, 100, 20, 80, 0));
-		
+
+		//Zone 3, total num=10
+		lots.add(new ParkingLot(32, LOT, ZONE3, "Olin Hall", "2400 Highland Ave", 36.142937, -86.805072, 20, 0, 20, 0));
+		lots.add(new ParkingLot(33, GARAGE, ZONE3, "25th Ave Staff Garage", "2499 Highland Ave", 36.142365, -86.806124, 100, 20, 80, 0));
+		lots.add(new ParkingLot(34, GARAGE, ZONE3, "Kensington Garage", "", 36.147163,-86.807613, 20, 0, 20, 0));
+		lots.add(new ParkingLot(35, LOT, ZONE3, "Parmer Field House", "", 36.145984,-86.808558, 20, 0, 20, 0));
+		lots.add(new ParkingLot(36, LOT, ZONE3, "Tarpley Bldg", "", 36.148549,-86.807699, 20, 0, 20, 0));
+		lots.add(new ParkingLot(37, LOT, ZONE3, "Delta Kappa Epsilon", "", 36.148618,-86.80624, 20, 0, 20, 0));
+		lots.add(new ParkingLot(38, LOT, ZONE3, "PI Kappa Alpha", "", 36.147786,-86.806927, 20, 0, 20, 0));
+		lots.add(new ParkingLot(39, LOT, ZONE3, "Lambda Chi Alpha", "", 36.147925,-86.805897, 20, 0, 20, 0));
+		lots.add(new ParkingLot(40, LOT, ZONE3, "Kensington Pl", "", 36.147578,-86.805296, 20, 0, 20, 0));
+		lots.add(new ParkingLot(41, LOT, ZONE3, "VANDERBILT PLACE", "", 36.144356,-86.809974, 20, 0, 20, 0));
+
 		//Zone 4, total num=4
-		lots.add(new ParkingLot(34, LOT, ZONE4, "", "2189-2199 Belcourt Ave", 36.136956,-86.805073, 20, 0, 20, 0));		
+		lots.add(new ParkingLot(34, LOT, ZONE4, "", "2189-2199 Belcourt Ave", 36.136956,-86.805073, 20, 0, 20, 0)); 
 		lots.add(new ParkingLot(35, LOT, ZONE4, "", "1203 17th Ave S", 36.142995,-86.794862, 20, 0, 20, 0));
 		lots.add(new ParkingLot(36, GARAGE, ZONE4, "1207 17th Ave Garage", "1207 17th Ave", 36.142428,-86.795034, 20, 0, 20, 0));
-		lots.add(new ParkingLot(37, GARAGE, ZONE4, "Real Estate", "", 36.144538,-86.795592, 20, 0, 20, 0));		
-		
-		//Medical
-		lots.add(new ParkingLot(38, LOT, MEDICAL, "Natchez field", "2964 Dudley Ave", 36.141120, -86.812059, 100, 20, 80, 0));
-		lots.add(new ParkingLot(39, GARAGE, MEDICAL, "Central garage", "2401 Highland Ave", 36.141924,-86.805909, 100, 20, 80, 0));
-		lots.add(new ParkingLot(40, GARAGE, MEDICAL, "South garage", "2376 Children's Way", 36.139454,-86.804375, 100, 20, 80, 0));
-		lots.add(new ParkingLot(41, GARAGE, MEDICAL, "Medical center East South Tower", "1211 Medical Center Dr", 36.141672, -86.800920, 100, 20, 80, 0));
-		lots.add(new ParkingLot(42, GARAGE, MEDICAL, "Vanderbilt Clinic garage", "1499 21st Ave S", 36.140381, -86.800899, 100, 20, 80, 0));
-		
-		//Visitor
-		lots.add(new ParkingLot(43, GARAGE, VISITOR, "South garage", "1598 24th Ave S", 36.139324, -86.804879, 50, 10, 40, 0.75));
-		lots.add(new ParkingLot(44, GARAGE, VISITOR, "Wesley Place Garage", "1901-2035 Scarritt Pl", 36.145736,-86.79871, 20, 0, 20, 0.75));
-		
+		lots.add(new ParkingLot(37, GARAGE, ZONE4, "Real Estate", "", 36.144538,-86.795592, 20, 0, 20, 0));
+
+		//Medical, total num=12
+		lots.add(new ParkingLot(38, LOT, MEDICAL, "Natchez Field", "2964 Dudley Ave", 36.141120, -86.812059, 100, 20, 80, 0));
+		lots.add(new ParkingLot(39, GARAGE, MEDICAL, "Central Garage", "2401 Highland Ave", 36.141924,-86.805909, 100, 20, 80, 0));
+		lots.add(new ParkingLot(40, GARAGE, MEDICAL, "South Garage", "2376 Children's Way", 36.139454,-86.804375, 100, 20, 80, 0));
+		lots.add(new ParkingLot(41, GARAGE, MEDICAL, "Medical Center East South Tower", "1211 Medical Center Dr", 36.141672, -86.800920, 100, 20, 80, 0));
+		lots.add(new ParkingLot(42, GARAGE, MEDICAL, "Vanderbilt Clinic Garage", "1499 21st Ave S", 36.140381, -86.800899, 100, 20, 80, 0));
+		lots.add(new ParkingLot(43, GARAGE, MEDICAL, "Medical Center East North Tower", "", 36.143628,-86.800489, 100, 20, 80, 0));
+		lots.add(new ParkingLot(44, LOT, MEDICAL, "Eskind Biomedical Library", "", 36.144182,-86.802292, 100, 20, 80, 0));
+		lots.add(new ParkingLot(45, LOT, MEDICAL, "Preston Research", "", 36.140751,-86.802818, 100, 20, 80, 0));
+		lots.add(new ParkingLot(46, LOT, MEDICAL, "Belcourt Child Care", "", 36.137086,-86.80389, 100, 20, 80, 0));
+		lots.add(new ParkingLot(47, LOT, MEDICAL, "", "2147 Blakemore Ave", 36.137615,-86.804395, 100, 20, 80, 0));
+		lots.add(new ParkingLot(48, LOT, MEDICAL, "", "2120 Belcourt Ave", 36.136991,-86.802378, 100, 20, 80, 0));
+		lots.add(new ParkingLot(49, LOT, MEDICAL, "Dayani Center", "", 36.140717,-86.800919, 100, 20, 80, 0));
+
+		//Visitor, total num=6
+		lots.add(new ParkingLot(50, GARAGE, VISITOR, "South Garage", "1598 24th Ave S", 36.139324, -86.804879, 50, 10, 40, 0.75));
+		lots.add(new ParkingLot(51, GARAGE, VISITOR, "Wesley Place Garage", "1901-2035 Scarritt Pl", 36.145736,-86.79871, 20, 0, 20, 0.75));
+		lots.add(new ParkingLot(52, LOT, VISITOR, "Schulman Center", "", 36.14481,-86.806256, 20, 0, 20, 0.75));
+		lots.add(new ParkingLot(53, LOT, VISITOR, "Jess Neely Dr", "", 36.143333,-86.807935, 20, 0, 20, 0.75));
+		lots.add(new ParkingLot(54, LOT, VISITOR, "Garland Ave", "", 36.143316,-86.805511, 20, 0, 20, 0.75));
+		lots.add(new ParkingLot(55, LOT, VISITOR, "24th Ave S", "", 36.142787,-86.804749, 20, 0, 20, 0.75));
 	}
 
 	// Query the database for parking lot by id.
@@ -178,34 +196,34 @@ public class ParkingDBManager
 		c.close();
 		return lotInZone;
 	}
-	
+
 	// Update database information based on parking lot unique ID number.
 	// !! Right now doesn't support inserting or deleting entry in DB.
 	public boolean updateDB(JSONArray servResp)
 	{
 		try {
 			for (int i = 0; i < servResp.length(); i++)
-		    {
-			    JSONObject lot = (JSONObject) servResp.get(i);
-			    int id = lot.getInt("id");
-			    ParkingLot p = queryParkingById(id);
-			    if (p != null)
-			    {
-			    	int num = lot.getInt("available");
-			    	// Database consistency check.
-			    	if (num >=0 && num < p.getNumSpot())
-			    	{
-			    		adapter.updateLot(id, num, num);
-				    	Log.i("i", "Updating entry :"+Integer.toString(id));
-			    	}
-			    	else
-			    		Log.i("i", "Error! Exceed the capacity of entry: "+Integer.toString(id));
-			    }
-			    else
-			    {
-			    	Log.i("i", "Entry not found in DB");
-			    }
-		    }
+			{
+				JSONObject lot = (JSONObject) servResp.get(i);
+				int id = lot.getInt("id");
+				ParkingLot p = queryParkingById(id);
+				if (p != null)
+				{
+					int num = lot.getInt("available");
+					// Database consistency check.
+					if (num >=0 && num < p.getNumSpot())
+					{
+						adapter.updateLot(id, num, 0);
+						Log.i("i", "Updating entry :"+Integer.toString(id));
+					}
+					else
+						Log.i("i", "Error! Exceed the capacity of entry: "+Integer.toString(id));
+				}
+				else
+				{
+					Log.i("i", "Entry not found in DB");
+				}
+			}
 		}catch (JSONException e)
 		{
 			e.printStackTrace();
