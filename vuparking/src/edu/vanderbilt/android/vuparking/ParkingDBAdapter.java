9c74328826a70d08f41a90bf59531a93bd6fd763
yuanzh14 <yuanzh14@ba21aae4-09fa-11df-bd4f-1fc3cb39d6f8>
2010-04-15 17:26:44 +0000

    US6:1 Database modified

--- vuparking/src/edu/vanderbilt/android/vuparking/ParkingDBAdapter.java
+++ vuparking/src/edu/vanderbilt/android/vuparking/ParkingDBAdapter.java
@@ -39,20 +39,33 @@ public class ParkingDBAdapter
 	public static final String KEY_AVAILABLE = "available";
 	public static final String KEY_DISABLE = "disable";
 	public static final String KEY_RATE = "rate";
+	
+	public static final String KEY_BD_ID = "bd_id";
+	public static final String KEY_BD_NAME = "bd_name";
+	public static final String KEY_BD_ADDR = "bd_addr";
+	public static final String KEY_BD_LOC_X = "bd_loc_x";
+	public static final String KEY_BD_LOC_Y = "bd_loc_y";
 
 	private static final String TAG = "ParkingDBAdapter";
 
 	private static final String DATABASE_NAME = "vuparking.db";
-	private static final String DATABASE_TABLE = "ParkingLot";
+	private static final String DATABASE_TABLE_PARKINGLOT = "ParkingLot";	
+	private static final String DATABASE_TABLE_BUILDING = "Building";
+	
 	private static final int DATABASE_VERSION = 1;
 
-	private static final String DATABASE_CREATE =
+	private static final String DATABASE_CREATE_PARKINGLOT =
 		"create table if not exists ParkingLot (_id integer primary key, "
 		+ "type integer not null, zone integer not null, "
 		+ "name text not null, address text, "
 		+ "coordinate_x text not null, coordinate_y text not null, "
 		+ "capacity integer not null, available integer not null,"
 		+ "disable integer not null, rate integer);";
+	
+	private static final String DATABASE_CREATE_BUILDING = 
+		"create table if not exists Building (bd_id integer primary key, "
+		+ "bd_name text not null, bd_addr text, "
+		+ "bd_loc_x text not null, bd_loc_y text not null);";
 
 	private final Context context;    
 	private DatabaseHelper DBHelper;
@@ -77,7 +90,8 @@ public class ParkingDBAdapter
 		public void onCreate(SQLiteDatabase db)
 		{
 			Log.i("i", "Creating Database");
-			db.execSQL(DATABASE_CREATE);
+			db.execSQL(DATABASE_CREATE_PARKINGLOT);
+			db.execSQL(DATABASE_CREATE_BUILDING);
 		}
 
 		@Override
@@ -87,6 +101,7 @@ public class ParkingDBAdapter
 					+ " to "
 					+ newVersion + ", which will destroy all old data");
 			db.execSQL("DROP TABLE IF EXISTS ParkingLot");
+			db.execSQL("DROP TABLE IF EXISTS Building");
 			onCreate(db);
 		}
 	}    
@@ -108,10 +123,13 @@ public class ParkingDBAdapter
 	// Check if there exist parking data.
 	public boolean checkDb()
 	{
-		Cursor c = db.rawQuery("select * from ParkingLot", null);
-		int count = c.getCount();
-		c.close();
-		if (count != 0) return true;
+		Cursor c1 = db.rawQuery("select * from ParkingLot", null);
+		Cursor c2 = db.rawQuery("select * from Building", null);
+		int count1 = c1.getCount();
+		int count2 = c2.getCount();
+		c1.close();
+		c2.close();
+		if (count1 != 0 && count2 != 0) return true;
 		else return false;
 	}    
 
@@ -130,7 +148,19 @@ public class ParkingDBAdapter
 		initialValues.put(KEY_AVAILABLE, p.getNumAvailabe());
 		initialValues.put(KEY_DISABLE, p.getNumDisable());
 		initialValues.put(KEY_RATE, p.getRate());
-		return db.insert(DATABASE_TABLE, null, initialValues);
+		return db.insert(DATABASE_TABLE_PARKINGLOT, null, initialValues);
+	}
+	
+	// Insertion: insert a new building into database, return -1 to indicate failure.
+	public long insertBuilding(Building b)
+	{
+		ContentValues initialValues = new ContentValues();
+		initialValues.put(KEY_BD_ID, b.getId());
+		initialValues.put(KEY_BD_NAME, b.getName());
+		initialValues.put(KEY_BD_ADDR, b.getAddress());
+		initialValues.put(KEY_BD_LOC_X, Double.toString(b.getLatitude()));
+		initialValues.put(KEY_BD_LOC_Y, Double.toString(b.getLongtitude()));
+		return db.insert(DATABASE_TABLE_BUILDING, null, initialValues);
 	}
 
 	// Update: update number of available and disable spots got from server.
@@ -140,28 +170,35 @@ public class ParkingDBAdapter
 		ContentValues args = new ContentValues();
 		args.put(KEY_AVAILABLE, num_available);
 		args.put(KEY_DISABLE, num_disable);
-		return db.update(DATABASE_TABLE, args,
+		return db.update(DATABASE_TABLE_PARKINGLOT, args,
 				KEY_ROWID + "=" + rowId, null) > 0;
 	}
 
 	// Deletion: deletes a particular parking lot from database.
 	public boolean deleteLot(long rowId)
 	{
-		return db.delete(DATABASE_TABLE, KEY_ROWID + 
+		return db.delete(DATABASE_TABLE_PARKINGLOT, KEY_ROWID + 
+				"=" + rowId, null) > 0;
+	}
+	
+	// Deletion: deletes a particular building from database.
+	public boolean deleteBuilding(long rowId)
+	{
+		return db.delete(DATABASE_TABLE_BUILDING, KEY_BD_ID + 
 				"=" + rowId, null) > 0;
 	}
 
 	// deletes all parking lots from database.
 	public boolean deleteAllLots()
 	{
-		return db.delete(DATABASE_TABLE, null, null)>0;
+		return db.delete(DATABASE_TABLE_PARKINGLOT, null, null)>0;
 	}
 
 	// Query: retrieve parking lot information by zone.
 	public Cursor getLotsByZone(int zone)
 	{
 		Cursor mCursor = 
-			db.query(false, DATABASE_TABLE, new String[]
+			db.query(false, DATABASE_TABLE_PARKINGLOT, new String[]
 			{
 				KEY_ROWID,
 				KEY_TYPE,
@@ -187,7 +224,7 @@ public class ParkingDBAdapter
 	public Cursor getLotById(long rowId) throws SQLException 
 	{
 		Cursor mCursor =
-			db.query(true, DATABASE_TABLE, new String[] 
+			db.query(true, DATABASE_TABLE_PARKINGLOT, new String[] 
 			{
 				KEY_ROWID,
 				KEY_TYPE,
@@ -202,6 +239,26 @@ public class ParkingDBAdapter
 				KEY_RATE,
 			}, 
 			KEY_ROWID + "=" + rowId, null, null, null, null, null);
+
+		if (mCursor != null)
+			mCursor.moveToFirst();
+
+		return mCursor;
+	}
+	
+	// retrieve building coordinates by id.
+	public Cursor getBuildingById(long rowId) throws SQLException 
+	{
+		Cursor mCursor =
+			db.query(true, DATABASE_TABLE_BUILDING, new String[] 
+			{
+				KEY_BD_ID,
+				KEY_BD_NAME,
+				KEY_BD_ADDR,
+				KEY_BD_LOC_X,
+				KEY_BD_LOC_Y,
+			}, 
+			KEY_BD_ID + "=" + rowId, null, null, null, null, null);
 
 		if (mCursor != null)
 			mCursor.moveToFirst();
